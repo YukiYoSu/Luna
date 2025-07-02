0b49e7a714a531719964b6b23101927d909e7593
YukiYoSu <kcandlert@gmail.com>
2025-07-02 13:50:52 +0200

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Bot is running!"
+def index():
+    return "Bot is online!"
+
+@app.route('/healthz')
+def health_check():
+    return "ok", 200
 
 def run():
     app.run(host='0.0.0.0', port=8080)
